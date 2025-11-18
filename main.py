514b293265ccc803fc84209878dc59109c95f202
João Pedro Thomaz <eduvaisersolado@gmail.com>
2025-11-18 12:31:17 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,55 +9,27 @@ io = Io()
 bus = Bus(mem, io)
 cpu = CPU(bus)
 
-# ============================
-# Programa de teste: 
-# Escrever "HELLO WORLD\n" na VRAM
-# usando instruções RISC-V
-# ============================
 
-# Endereço base da VRAM
 VRAM_BASE = 0x80000
 
-# String que vamos escrever
-msg = b"HELLO WORLD\n"
 
-# Gerar um pequeno programa RISC-V:
-# t0 = VRAM_BASE
-# t1 = msg[i]
-# sb t1, i(t0)
-
-# Registradores:
-# t0 = x5
-# t1 = x6
-
-# --- Carregar endereço base da VRAM em t0 (x5) ---
-# LUI x5, upper(80000)
 upper = (VRAM_BASE >> 12) & 0xFFFFF
-mem.write32(0,  (upper << 12) | (5 << 7) | 0x37)  # LUI x5, upper
+mem.write32(0,  (upper << 12) | (5 << 7) | 0x37) 
 
-# --- Escrever cada caractere ---
 pc = 4
 for i, b in enumerate(msg):
-    # ADDI x6, x0, b      (t1 = caractere)
+   
     instr_addi = (b << 20) | (0 << 15) | (6 << 7) | (0x13)
     mem.write32(pc, instr_addi)
     pc += 4
 
-    # SB x6, i(x5)
     imm11_5 = (i >> 5) & 0x7F
     imm4_0  = i & 0x1F
     instr_sb = (imm11_5 << 25) | (6 << 20) | (5 << 15) | (0x0 << 12) | (imm4_0 << 7) | 0x23
     mem.write32(pc, instr_sb)
     pc += 4
 
-# Loop infinito para não sair do programa
-# JAL x0, 0
 mem.write32(pc, 0x0000006F)
-
-
-# ============================
-# EXECUÇÃO DO CPU
-# ============================
 
 print("Executando...\n")
 
